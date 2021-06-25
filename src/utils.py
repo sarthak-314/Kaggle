2e4e21cbbab64071a5a93acede562093688b88a1
Sarthak <sarthak.bhatt314@gmail.com>
2021-06-25 13:01:18 +0530

    lazy

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 SRC_PACKAGE_PATH = Path('C:\\Users\\sarth\\Desktop\\kaggle-v2\\src')
 INPUT_PATH = Path('C:\\Users\\sarth\\Desktop\\kaggle-v2')
+COMP_NAME = 'siim-covid19-detection'
